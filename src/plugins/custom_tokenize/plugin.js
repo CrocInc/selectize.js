24af87bd1e779c278215240b450dfe7275bf46ec
Vyguzov Aleksandr <avyguzov@croc.ru>
2020-02-04 19:32:18 +0800

    custom_tokenize plugin

---
+++ src/plugins/custom_tokenize/plugin.js
@@ -0,0 +1,21 @@
+/**
+ * Plugin: "custom_tokenize" (selectize.js)
+ * Copyright (c) 2020 Aleksandr Vyguzov & contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at:
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+ * ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ *
+ * @author Aleksandr Vyguzov <avgyzov@croc.ru>
+ */
+
+Selectize.define('custom_tokenize', function (options) {
+    if (options.tokenize) {
+        this.sifter.tokenize = options.tokenize;
+    }
+});
